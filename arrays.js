0aae53d21ebd29f6e8d6c4db30214131625010b7
slub69 <slublins2@gmail.com>
2023-10-05 10:15:57 -0400

    10/5/23

---
+++ arrays.js
@@ -0,0 +1,25 @@
+const characters = [Spongebob, Patrick, Squiqward, Krabs]
+const Spongebob = {
+    fname: 'Spongebob',
+    lname: 'Squarepants',
+    age: 18,
+    parents: ['Harold Squarepants', 'Margret Squarepants']
+}
+const Patrick = {
+    fname: 'Patrick',
+    lname: 'Star',
+    age: 37,
+    parents: ['Herb Star', 'Margie Star']
+}
+const Squiqward = {
+    fname: 'Squidward',
+    lname: 'Tentacles',
+    age: 30,
+    parents: ['Jeff Tentacles', 'Mrs. Tentacles']
+}
+const Krabs = {
+    fname: 'Eugene',
+    lname: 'Krabs',
+    age: 18,
+    parents: ['Harold Squarepants', 'Margret Squarepants']
+}
