1fd66943e3881a7d6babff0263dabed7b5d73dbb
slub69 <slublins2@gmail.com>
2023-10-10 09:51:44 -0400

    10/10/23

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ const characters = [
     Spongebob = {
         fname: 'Spongebob',
         lname: 'Squarepants',
-        age: 18,
+        age: 17,
         parents: ['Harold Squarepants', 'Margret Squarepants']
 },
  Patrick = {
@@ -21,13 +21,11 @@ const characters = [
  Krabs = {
     fname: 'Eugene',
     lname: 'Krabs',
-    age: 18,
+    age: 17,
     parents: ['Krabs Senior', 'Betsy Krabs']
 }]
-characters.forEach((name)=>
-    console.log(name.fname)
-)
-const young = characters.filter((old)=>
-    old.age < 21
-)
-console.log(young);
+characters.forEach((name)=>console.log(name.fname))
+characters.forEach((parent)=>console.log(parent.fname + '\'s dad is ' + parent.parents[0] + '.'))
+characters.forEach((parent)=>console.log(parent.fname + '\'s mom is ' + parent.parents[1] + '.'))
+const young = characters.filter((old)=>old.age < 18)
+young.forEach((young_name)=>console.log(young_name.fname + ' is a minor.'))
